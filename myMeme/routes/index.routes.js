0e43d02dc22666adaf737893508cea06eec7dcc8
JB <josepbardesp@gmail.com>
2022-10-16 14:40:22 +0200

    Primer planteamiento de la NAV bar

--- myMeme/routes/index.routes.js
+++ myMeme/routes/index.routes.js
@@ -6,4 +6,14 @@ router.get("/", (req, res, next) => {
   res.render("index");
 });
 
+/* GET login page */
+router.get("/login", (req, res, next) => {
+  res.render("login");
+});
+
+/* GET Sign in page */
+router.get("/signin", (req, res, next) => {
+  res.render("signin");
+});
+
 module.exports = router;
