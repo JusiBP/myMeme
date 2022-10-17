9f2c1b647883a712d97c227806709ae1f2996159
Jordi Martinez <jordiupc87@gmail.com>
2022-10-17 18:35:34 +0200

    Create 50% done, not writing on DB yet

--- myMeme/models/Post.model.js
+++ myMeme/models/Post.model.js
@@ -1,25 +1,34 @@
-
 const { Schema, model } = require("mongoose");
 
-const postSchema = new Schema(
-  {
-    username: {type: Schema.Types.ObjectId, ref: "User"},
-    memeUrl: {
-        type: String,
-        require: true
-    },
-    date: {
-      type: Date, 
-      default: Date.now,
-    },
-    description: {
-        type: String,
-        require: false
-    },
-    // this second object adds extra properties: `createdAt` and `updatedAt`
-    // timestamps: true,
-  }
-);
+const postSchema = new Schema({
+  username: { type: Schema.Types.ObjectId, ref: "User" },
+  memeUrl: {
+    type: String,
+    require: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+  description: {
+    type: String,
+    require: false,
+  },
+  category: {
+    enum: [
+      "Random",
+      "Animals",
+      "Wtf",
+      "Sports",
+      "Gaming",
+      "Politics",
+      "News",
+      "Anime&Manga",
+    ],
+  },
+  // this second object adds extra properties: `createdAt` and `updatedAt`
+  // timestamps: true,
+});
 
 const Post = model("Post", postSchema);
 
--- myMeme/routes/user.routes.js
+++ myMeme/routes/user.routes.js
@@ -1,3 +1,29 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
 
+// // Require the User model in order to interact with the database
+// const User = require("../models/User.model");
+// const Post = require("../models/Post.model");
+
+// // Require necessary (isLoggedOut and isLiggedIn) middleware in order to control access to specific routes
+// const isLoggedOut = require("../middleware/isLoggedOut");
+// const isLoggedIn = require("../middleware/isLoggedIn");
+
+router.get("/createpost", (req, res, next) => {
+  res.render("createPost");
+});
+
+router.post("/createpost", (req, res, next) => {
+  Post.create(req.body)
+    .then((result) => {
+      res.render("index");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+
+  console.log(" ----- Post created!!!! ----- ");
+});
+
+module.exports = router;
