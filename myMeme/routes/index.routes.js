ae96a2fd9f782592efc41fb01639e8e54256767e
Jordi Martinez <jordiupc87@gmail.com>
2022-10-17 14:59:34 +0200

    just in case

--- myMeme/routes/index.routes.js
+++ myMeme/routes/index.routes.js
@@ -1,18 +1,21 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 /* GET home page */
 router.get("/", (req, res, next) => {
+  console.log("Hello Index");
   res.render("index");
 });
 
 /* GET login page */
 router.get("/login", (req, res, next) => {
+  console.log("Hello login");
   res.render("auth/login");
 });
 
 /* GET Sign in page */
 router.get("/signin", (req, res, next) => {
+  console.log("Hello signin");
   res.render("auth/signin");
 });
 
