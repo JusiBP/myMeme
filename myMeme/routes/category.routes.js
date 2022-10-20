b29a4609ab6a1925a6a964502fd09200eb80a1a7
JB <josepbardesp@gmail.com>
2022-10-20 16:05:33 +0200

    categories done & routes depuration being done

--- myMeme/routes/category.routes.js
+++ myMeme/routes/category.routes.js
@@ -1,28 +1,22 @@
 const express = require("express");
 const Post = require("../models/Post.model");
 const router = express.Router();
+const dateFunction = require ("../utils/date.function.js")
 
-/* GET Category */
 router.get("/:category", (req, res, next) => {
-    console.log ("EN RUTA! params:", req.params )
     if (req.params.category === "All"){
         res.redirect("/");
     }
     else{
         Post.find({category: req.params.category})
+        .populate("userInfo")
         .then(post => {
-            console.log("hola desde category posts: ",post)
-            //   post.category = "WTF"
-            if (req.session.currentUser) {
-            const {username} = req.session.currentUser
-            res.render("index", {username: username, post});
-            }
-            else {
-            res.render("index", {post});
-        }
+        let dataViews = dateFunction(post, req.session.currentUser);
+        dataViews.category = req.params.category
+        console.log ("EN RUTA! dataViews:", dataViews )
+        res.render("index", dataViews);
         })
     }
 });
-  
 
 module.exports = router;
